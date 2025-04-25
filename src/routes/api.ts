67433ebded787293905ecdb469989fab04f8fa95
Glenford999 <lwilliamsx86@outlook.com>
2025-04-25 20:23:14 +0100

    Delete src directory

--- src/routes/api.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { testDeepSeekConnection } from '../controllers/testController';
-
-const router = Router();
-
-// Test DeepSeek API connection
-router.get('/test/deepseek', testDeepSeekConnection);
-
-export default router; 
